1d0d29170a99293dffe042945445c4d325bb162b
W-46ec <30034417+W-46ec@users.noreply.github.com>
2017-08-18 16:57:51 +0800

    更换邮件授权码

--- tools/mail.js
+++ tools/mail.js
@@ -4,15 +4,15 @@ var transporter = nodemailer.createTransport({
 	port: 465, // SMTP 端口
 	secureConnection: true, // 使用 SSL
 	auth: {
-		user: '3206434808@qq.com',
-		pass: 'qkhoobpspuxodeda'
+		user: '2015197295@qq.com',
+		pass: 'mpvsweyuqzridjcb'
 	}
 });
 
 //发送邮件
 var sendEmail = function(mail, msg, cb){
 	var mailOptions = {
-		from: '3206434808@qq.com',
+		from: '2015197295@qq.com',
 		to: mail,
 		subject: 'Daily CSS 验证码', // 标题
 		html: msg // html 内容
