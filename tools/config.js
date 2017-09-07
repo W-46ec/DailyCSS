646bf71193df285df56ac2dc2354b6e0313baa10
W-46ec <30034417+W-46ec@users.noreply.github.com>
2017-09-07 21:32:12 +0800

    开启不过期Token模式

--- tools/config.js
+++ tools/config.js
@@ -1,23 +1,23 @@
 var sha256 = require('sha256');
 var uuid = require('uuid');
 
-var key = sha256(uuid.v4() + (+(new Date())).toString());
-var expires = {
-	expiresIn: '600000'
-};
+// var key = sha256(uuid.v4() + (+(new Date())).toString());
+// var expires = {
+// 	expiresIn: '600000'
+// };
 
-var urlConfig = 'http://xxx:3000';
+// var urlConfig = 'http://xxx:3000';
 
-var registerEmailFrequency = 120000;
+// var registerEmailFrequency = 120000;
 
 
 //developer mode
-// var key = "key";
-// var expires = {};
+var key = "key";
+var expires = {};
 
-// var urlConfig = 'http://127.0.0.1:3000';
+var urlConfig = 'http://127.0.0.1:3000';
 
-// var registerEmailFrequency = 0;
+var registerEmailFrequency = 120000;
 
 module.exports = {
 	key: key,
